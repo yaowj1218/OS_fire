b5c24beed56ed058bc9380e718ae307195cc6504
姚文杰 <yaowenjie@yaowenjiedembp.lan>
2016-12-04 14:47:09 +0800

    Initial Commit

---
+++ OS_fire/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OS_fire
+//
+//  Created by 姚文杰 on 2016/12/4.
+//  Copyright © 2016年 姚文杰. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
